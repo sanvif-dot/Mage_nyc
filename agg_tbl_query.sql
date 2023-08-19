9d78122d95357ae7d5ca3f7d3a13f503fcd288dd
sanvif-dot <122470555+sanvif-dot@users.noreply.github.com>
2023-08-19 13:39:48 +0700

    first commit

---
+++ agg_tbl_query.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE TABLE `magnetic-hawk-391614.nyc_mage.agg_tbl` AS (
+SELECT 
+    
+    f.VendorID,
+    d.tpep_pickup_datetime,
+    d.tpep_dropoff_datetime,
+    p.passenger_count,
+    t.trip_distance,
+    r.rate_code_name,
+    pick.pickup_latitude,
+    pick.pickup_longitude,
+    drop.dropoff_latitude,
+    drop.dropoff_longitude,
+    pay.payment_type_name,
+    f.fare_amount,
+    f.extra,
+    f.mta_tax,
+    f.tip_amount,
+    f.tolls_amount,
+    f.improvement_surcharge,
+    f.total_amount
+FROM 
+    `magnetic-hawk-391614.nyc_mage.fact_table` f
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.datetime_dim` d  ON f.datetime_id = d.datetime_id
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.passenger_count_dim` p  ON p.passenger_count_id = f.passenger_count_id  
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.trip_distance_dim` t  ON t.trip_distance_id = f.trip_distance_id  
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.rate_code_dim` r ON r.rate_code_id = f.rate_code_id  
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.pickup_location_dim` pick ON pick.pickup_location_id = f.pickup_location_id
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.dropoff_location_dim` drop ON drop.dropoff_location_id = f.dropoff_location_id
+JOIN 
+    `magnetic-hawk-391614.nyc_mage.payment_type_dim` pay ON pay.payment_type_id = f.payment_type_id
+);
